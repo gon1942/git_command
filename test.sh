ddd8ad91532c4b4a892e8175e92ae1ff62a4c0c7
김경곤 <kg1942@invesume.com>
2021-08-11 11:26:19 +0900

    test

---
+++ test.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# .git/hooks/prepare-commit-msg
+#
+# Automatically add branch name and branch description to every commit message except merge commit.
+# https://stackoverflow.com/a/18739064
+#
+
+COMMIT_EDITMSG=$1
+echo $COMMIT_EDITMSG
+
+addBranchNumber() {
+  NAME=$(git branch | grep '*' | sed 's/* //') 
+  echo $NAME
+  ISSUE_NUMBER=`echo $NAME | cut -d '-' -f1`
+  echo $ISSUE_NUMBER
+  DESCRIPTION=$(git config branch."$NAME".description)
+  echo "[#$ISSUE_NUMBER] $(cat $COMMIT_EDITMSG)" > $COMMIT_EDITMSG
+  if [ -n "$DESCRIPTION" ] 
+  then
+     echo "" >> $COMMIT_EDITMSG
+     echo $DESCRIPTION >> $COMMIT_EDITMSG
+  fi 
+}
+
+MERGE=$(cat $COMMIT_EDITMSG|grep -i 'merge'|wc -l)
+
+if [ $MERGE -eq 0 ] ; then
+  addBranchNumber
+fi
